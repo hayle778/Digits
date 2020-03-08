cc536766d0aa984273c6c23d9b9ad599442e0aa6
Haylemelekot <HayNaS@Vickis-iMac.local>
2020-03-08 18:38:27 -0400

    ff

---
+++ src/Soccer.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Soccer {
+public static void main(String[]args) {
+
+    //Scanner input = new Scanner(System.in);
+     int a = 1;
+     int b = 2;
+     int c = 3;
+     int d = 4;
+     int e = 5;
+     int f = 6;
+     int g = 7;
+     int h = 8;
+     int i = 9;
+     int j = 10;
+
+     System.out.println("The first 10 natural numbers are: ");
+     System.out.println("" + a );
+     System.out.println("" + b );
+     System.out.println("" + c );
+     System.out.println("" + d );
+     System.out.println("" + e);
+     System.out.println("" + f );
+     System.out.println("" + g );
+     System.out.println("" + h );
+     System.out.println("" + i );
+     System.out.println("" + j );
+
+
+System.out.println("----------------------------");
+     System.out.println("Enter the first digit: ");
+
+     for(i = 1; i < 11; i++){
+      System.out.println("Order: "+ i);
+
+
+     }
+
+}
+
+}
